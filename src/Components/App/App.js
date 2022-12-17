82a6b8e44b5890c99791e714ca97a9459c4d470d
Paul G <35400575+notpaulgaddy@users.noreply.github.com>
2022-12-17 11:41:25 -0500

    Add files via upload

---
+++ src/Components/App/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import SearchBar from "../SearchBar/SearchBar";
+import Playlist from "../Playlist/Playlist";
+import SearchResults from '../SearchResults/SearchResults';
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{name: "name1",artist:"artist1",album:"album1",id:1},{name: "name3",artist:"artist3",album:"album3",id:3}]
+    }
+  }
+  render(){
+    return(
+      <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div class="App">
+    <SearchBar/>
+    <div className="App-playlist">
+      <SearchResults searchResults={this.state.searchResults}/>
+      <Playlist/>
+    </div>
+  </div>
+</div>
+    )
+  }
+}
+
+export default App;
